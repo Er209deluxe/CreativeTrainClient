561319ae39b1cd7acc9e4c1099f2b7e0e1cb39ac
Er209deluxe <frngtffet@gmail.com>
2026-07-02 21:10:42 +0200

    Update

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Material App',
+      title: 'Creative Train',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const Scaffold(body: Center(child: Text('Welcome to Flutter'))),
     );
