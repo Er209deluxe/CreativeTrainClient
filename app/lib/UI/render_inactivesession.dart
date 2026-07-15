baafe4ff2159a8fabcc0b7ab647206153a30f6ba
= <=>
2026-07-15 11:39:38 +0200

    Added copy UUID button

--- app/lib/UI/render_inactivesession.dart
+++ app/lib/UI/render_inactivesession.dart
@@ -5,6 +5,7 @@ import 'package:creativetrainclient/UI/render_registerconfig.dart';
 import 'package:creativetrainclient/configs/UI/standartm3edesign.dart';
 import 'package:flutter/material.dart';
 import 'package:m3e_buttons/m3e_buttons.dart';
+import 'package:flutter/services.dart';
 
 class RenderInactivesession extends StatefulWidget {
   const RenderInactivesession({super.key});
@@ -25,67 +26,167 @@ class _RenderInactivesessionState extends State<RenderInactivesession> {
             child: Column(
               children: [
                 const SizedBox(height: 35),
-                if (app_state.getCurrentSession().isHost)
-                  M3EButton(
-                    onPressed: () {
-                      print("start session");
-                      startSession("temp role configR");
-                    },
-                    decoration: M3EButtonDecoration.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 13, 188, 0),
-                      foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                    ),
-                    size: M3EButtonSize.custom(height: 65, width: 550),
-                    child: const Text(
-                      'Start Session',
-                      style: TextStyle(fontSize: 22),
-                    ),
-                  ),
-                const SizedBox(height: 20),
-                M3EButton(
-                  onPressed: () {
-                    //Leave game
-                    String? ipAddress = app_state.getIpAddress();
-                    if (ipAddress == null) {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext dialogContext) {
-                          return ErrorDialogM3E(
-                            errorHeader: 'Missing ip address',
-                            errorText: 'Ip address not found',
-                          );
-                        },
-                      );
-                      return;
-                    }
-                    leaveSession(
-                      ipAddress,
-                      app_state.getCurrentSession().playerUuid,
-                      app_state.getCurrentSession().token,
-                    );
+                Row(
+                  children: [
+                    const SizedBox(width: 40),
+                    if (app_state.getCurrentSession().isHost)
+                      Expanded(
+                        child: M3EButton(
+                          onPressed: () {
+                            print("start session");
+                            startSession("temp role configR");
+                          },
+                          decoration: M3EButtonDecoration.styleFrom(
+                            backgroundColor: const Color.fromARGB(
+                              255,
+                              13,
+                              188,
+                              0,
+                            ),
+                            foregroundColor: const Color.fromARGB(
+                              255,
+                              255,
+                              255,
+                              255,
+                            ),
+                          ),
+                          size: M3EButtonSize.custom(height: 65, width: 550),
+                          child: const Text(
+                            'Start Session',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                      ),
 
-                    Navigator.pop(context);
-                  },
-                  decoration: M3EButtonDecoration.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 143, 3, 3),
-                    foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                  ),
-                  size: M3EButtonSize.custom(height: 65, width: 550),
-                  child: const Text(
-                    'Leave Game',
-                    style: TextStyle(fontSize: 22),
-                  ),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      child: M3EButton(
+                        onPressed: () {
+                          //Leave game
+                          String? ipAddress = app_state.getIpAddress();
+                          if (ipAddress == null) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext dialogContext) {
+                                return ErrorDialogM3E(
+                                  errorHeader: 'Missing ip address',
+                                  errorText: 'Ip address not found',
+                                );
+                              },
+                            );
+                            return;
+                          }
+                          leaveSession(
+                            ipAddress,
+                            app_state.getCurrentSession().playerUuid,
+                            app_state.getCurrentSession().token,
+                          );
+
+                          Navigator.pop(context);
+                        },
+                        decoration: M3EButtonDecoration.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 143, 3, 3),
+                          foregroundColor: const Color.fromARGB(
+                            255,
+                            255,
+                            255,
+                            255,
+                          ),
+                        ),
+                        size: M3EButtonSize.custom(height: 65, width: 550),
+                        child: const Text(
+                          'Leave Game',
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 40),
+                  ],
                 ),
+                if (app_state.getCurrentSession().sessionUuid != '')
+                  Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          const SizedBox(width: 35),
+                          Expanded(
+                            child: M3EButton(
+                              onPressed: () async {
+                                await Clipboard.setData(
+                                  ClipboardData(
+                                    text: app_state
+                                        .getCurrentSession()
+                                        .sessionUuid,
+                                  ),
+                                );
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text('Copied Session UUID'),
+                                  ),
+                                );
+                              },
+                              decoration: M3EButtonDecoration.styleFrom(
+                                backgroundColor: const Color.fromARGB(
+                                  255,
+                                  29,
+                                  27,
+                                  50,
+                                ),
+                                foregroundColor: const Color.fromARGB(
+                                  255,
+                                  255,
+                                  255,
+                                  255,
+                                ),
+                              ),
+                              size: M3EButtonSize.custom(
+                                height: 65,
+                                width: 550,
+                              ),
+                              child: Text(
+                                app_state.getCurrentSession().sessionUuid,
+                                style: TextStyle(fontSize: 22),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 35),
+                        ],
+                      ),
+                    ],
+                  ),
                 const SizedBox(height: 20),
-                M3EButton(
-                  onPressed: () {},
-                  decoration: M3EButtonDecoration.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 3, 59, 143),
-                    foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                  ),
-                  size: M3EButtonSize.custom(height: 65, width: 550),
-                  child: const Text('Players', style: TextStyle(fontSize: 22)),
+                Row(
+                  children: [
+                    const SizedBox(width: 35),
+                    Expanded(
+                      child: M3EButton(
+                        onPressed: () {},
+                        decoration: M3EButtonDecoration.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            3,
+                            59,
+                            143,
+                          ),
+                          foregroundColor: const Color.fromARGB(
+                            255,
+                            255,
+                            255,
+                            255,
+                          ),
+                        ),
+                        size: M3EButtonSize.custom(height: 65, width: 550),
+                        child: const Text(
+                          'Players',
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 35),
+                  ],
                 ),
+
                 //TODO: Render all Player names
               ],
             ),
